408b56da99ec59fe845878bff713032216cf5433
Aayush Agarwal <crazy_nuclei@Aayushs-MacBook-Air.local>
2025-03-25 23:25:46 +0530

    counter app with local state

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
-import App from './apps/Greeting.jsx'
+// import App from './apps/Greeting.jsx'
+import App from './apps/CounterApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
